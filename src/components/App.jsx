db481776535da92534c60479e5918725739f3229
Nick Leso <nickleso.work@gmail.com>
2022-10-29 12:23:59 +0300

    Relocate feedback widjet to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,61 @@
-import FeedbackWidjet from './FeedbackWidjet/FeedbackWidjet';
+import React, { Component } from 'react';
+import { Statistics } from './FeedbackWidjet/Statistics';
+import { FeedbackOptions } from './FeedbackWidjet/FeedbackOptions';
+import { Notification } from './FeedbackWidjet/Notification';
+import { Section } from './FeedbackWidjet/Section';
 
-export const App = () => {
-  return <FeedbackWidjet />;
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = event => {
+    this.setState(prevState => ({
+      [event.target.name]: prevState[event.target.name] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = good + neutral + bad;
+    return totalFeedback;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good, neutral, bad } = this.state;
+    let positiveFeedbackPercentage = 0;
+    positiveFeedbackPercentage = (good * 100) / (good + neutral + bad);
+    return parseInt(positiveFeedbackPercentage);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <Section title={'Please live feedback'}>
+          <FeedbackOptions onLeaveFeedback={this.addFeedback} />
+        </Section>
+
+        <Section style={{ backgroundColor: 'white' }} title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+
+        <Notification
+          message={'There is no feedback'}
+          total={this.countTotalFeedback()}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from 'components/App';
 import { Global, ThemeProvider } from '@emotion/react';
-import { App } from 'components/App';
 import './index.css';
 import { GlobalStyles, theme } from 'styles';
 
